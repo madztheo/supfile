775f80bcbd6708004a266f31448690c5cf4257dc
Unknown <rvogelsperger@gmail.com>
2018-05-15 12:45:04 +0200

    button login google
    partie client finit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,9 @@
-import { Component } from "@angular/core";
+import { Component, NgZone } from "@angular/core";
 import { Router } from "@angular/router";
+
+declare const gapi: any;
+
+
 
 @Component({
   selector: "supfile-login",
@@ -7,8 +11,30 @@ import { Router } from "@angular/router";
   styleUrls: ["./login.component.scss"]
 })
 export class LogInComponent {
-  constructor(private router: Router) {}
+constructor(private router: Router, ngZone: NgZone) {}
+auth2:any;
+	ngAfterViewInit() {
+     gapi.load('auth2',  () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '421233958586-jnug92ahf1fliq0v5ne35db88rja4rvd.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin(document.getElementById('googlelogin'));
+    });
+}
+	
+	attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (loggedInUser) => {  
+      console.log( loggedInUser);
+				this.router.navigate(["/"]);
 
+      }, function (error) {
+	console.log(error);
+      });
+ }
+	
   onSubmit(event: Event) {
     event.preventDefault();
     this.router.navigate(["/"]);
@@ -17,4 +43,7 @@ export class LogInComponent {
   signUp() {
     this.router.navigate(["/signup"]);
   }
+
+ 
+	
 }
