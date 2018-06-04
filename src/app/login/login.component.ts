77c5f0a204b5f9f7f8c94e2ee5b521b64bdcb73a
Unknown <rvogelsperger@gmail.com>
2018-06-04 09:31:07 +0200

    reglage du bug de redirection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,49 +1,58 @@
-import { Component, NgZone } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 declare const gapi: any;
 
 
-
 @Component({
-  selector: "supfile-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+    selector: 'supfile-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LogInComponent {
-constructor(private router: Router, ngZone: NgZone) {}
-auth2:any;
-	ngAfterViewInit() {
-     gapi.load('auth2',  () => {
-      this.auth2 = gapi.auth2.init({
-        client_id: '421233958586-jnug92ahf1fliq0v5ne35db88rja4rvd.apps.googleusercontent.com',
-        cookiepolicy: 'single_host_origin',
-        scope: 'profile email'
-      });
-      this.attachSignin(document.getElementById('googlelogin'));
-    });
-}
-	
-	attachSignin(element) {
-    this.auth2.attachClickHandler(element, {},
-      (loggedInUser) => {  
-      console.log( loggedInUser);
-				this.router.navigate(["/"]);
 
-      }, function (error) {
-	console.log(error);
-      });
- }
-	
-  onSubmit(event: Event) {
-    event.preventDefault();
-    this.router.navigate(["/"]);
-  }
+    constructor(public router: Router) {}
+    auth2: any;
+    ngAfterViewInit() {
+        gapi.load('auth2',  () => {
+            this.auth2 = gapi.auth2.init({
+                client_id: '421233958586-jnug92ahf1fliq0v5ne35db88rja4rvd.apps.googleusercontent.com',
+                cookiepolicy: 'single_host_origin',
+                scope: 'profile email'
+            });
+            this.attachSignin(document.getElementById('googlelogin'));
+        });
+    }
 
-  signUp() {
-    this.router.navigate(["/signup"]);
-  }
+    attachSignin(element) {
+        this.auth2.attachClickHandler(element, {},
+            (googleUser) => {
+                const profile = googleUser.getBasicProfile();
+                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                console.log('Name: ' + profile.getName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+                if (profile.getId() !== '' ) {
 
- 
-	
+
+                    window.location.assign('/dashboard');
+
+                }
+            }, function (error) {
+                console.log(error);
+            });
+    }
+
+    onSubmit(event: Event) {
+        event.preventDefault();
+        this.router.navigate(['/']);
+    }
+
+    signUp() {
+        this.router.navigate(['/signup']);
+    }
+
+
+
 }
