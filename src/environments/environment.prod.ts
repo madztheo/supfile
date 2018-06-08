b7a2e5df90738ab6ef440b8e433cb89f17c423b6
Théo Madzou <madztheo@hotmail.fr>
2018-06-08 16:04:30 +0200

    misc(update env variables)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverUrl: "http://localhost:1337",
-  //webAppUrl: "http://182.168.152.5",
+  serverUrl: "http://192.168.152.6:1337",
+  //webAppUrl: "http://192.168.152.5",
   parsePublicKey: "r2iHRgNfOM8lih4"
 };
